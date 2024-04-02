ee3585f2d2d838f54576ec8d02d476ba5e85a075
Nguyen Long Duong <nguyenlongduong000@gmail.com>
2024-04-02 11:46:58 +0700

    Change App.js and add component AuthPage , CartPage, HomePage

---
+++ src/component/CartPage/CartPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CartPage() {
+  return (
+    <div>
+      <h1>CartPage</h1>
+      <p>Xin chào! Đây là trang chủ của chúng tôi.</p>
+    </div>
+  );
+}
+
+export default CartPage;
---
+++ src/component/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Trang Chủ</h1>
+      <p>Xin chào! Đây là trang chủ của chúng tôi.</p>
+    </div>
+  );
+}
+
+export default Homepage;
